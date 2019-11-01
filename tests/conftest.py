e292d6947b168ac474be25610c8f10ea2d326f26
Qiwei Liu qiwliu@iu.edu <github675lqw>
2019-10-31 21:29:45 -0400

    fix test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,42 +1,42 @@
-# import os
-# import tempfile
-# import pytest
-# import sys
-#
-#
-# # Add the path for configuring tests
-# # sys.path.append(os.path.join(os.path.dirname(os.getcwd()),'/cloudmesh'))
-#
-# from cloudmesh.analytics.server.server import create_app
-# from cloudmesh.analytics.server.db import get_db, init_db
-#
-#
-# @pytest.fixture
-# def app():
-#     print(sys.path)
-#     db_fd, db_path = tempfile.mkstemp()
-#
-#     app = create_app({
-#         'TESTING': True,
-#         'DATABASE': db_path,
-#         # Set the folder path for testing. All uploaded files will be saved here
-#         'UPLOAD_FOLDER': './testing_files'
-#     })
-#
-#     with app.app_context():
-#         init_db()
-#
-#     yield app
-#
-#     os.close(db_fd)
-#     os.unlink(db_path)
-#
-#
-# @pytest.fixture
-# def client(app):
-#     return app.test_client()
-#
-#
-# @pytest.fixture
-# def runner(app):
-#     return app.test_cli_runner()
+import os
+import tempfile
+import pytest
+import sys
+
+
+# Add the path for configuring tests
+# sys.path.append(os.path.join(os.path.dirname(os.getcwd()),'/cloudmesh'))
+
+from cloudmesh.analytics.server.server import create_app
+from cloudmesh.analytics.server.db import get_db, init_db
+
+
+@pytest.fixture
+def app():
+    print(sys.path)
+    db_fd, db_path = tempfile.mkstemp()
+
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path,
+        # Set the folder path for testing. All uploaded files will be saved here
+        'UPLOAD_FOLDER': './testing_files'
+    })
+
+    with app.app_context():
+        init_db()
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
