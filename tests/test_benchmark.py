d0de59a6999ed5d969724b3cfa82b715f5f96cb6
Yanting Wan <yanwan@indiana.edu>
2019-11-27 10:09:00 +0800

    write the skeleton code of generating benchmarks

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -6,53 +6,58 @@ from cloudmesh.common.StopWatch import StopWatch
 
 class TestBenchmark:
     @pytest.fixture
+    def ip_of_server(self):
+        pass
+
+    @pytest.fixture
     def generated_data_file(self):
         xs = np.random()
         ys = 3*xs + 1 + np.random()
         data_file = write(xs, ys)
         return data_file
 
-    def test_file_upload_benchmark(self, generated_data_file):
+    def test_file_upload_benchmark(self, generated_data_file, ip_of_server):
         StopWatch.start()
 
         StopWatch.stop()
         pass
 
-    def test_list_file_benchmark(self):
+    def test_list_file_benchmark(self, ip_of_server):
         StopWatch.start()
         StopWatch.stop()
         pass
 
-    def test_read_file_benchmark(self, filename):
+    def test_read_file_benchmark(self, generated_data_file, ip_of_server):
         StopWatch.start()
         StopWatch.stop()
         pass
 
-    def test_linear_consturctor_benchmark(self):
+    def test_linear_consturctor_benchmark(self, ip_of_server):
         StopWatch.start()
         StopWatch.stop()
         pass
 
-    def test_linear_fit_benchmark(self):
+    def test_linear_fit_benchmark(self, ip_of_server):
         StopWatch.start()
         StopWatch.stop()
         pass
 
-    def test_linear_get_params_benchmark(self):
+    def test_linear_get_params_benchmark(self, ip_of_server):
         StopWatch.start()
         StopWatch.stop()
         pass
 
-    def test_linear_predict_benchmark(self):
+    def test_linear_predict_benchmark(self, ip_of_server):
         StopWatch.start()
         StopWatch.stop()
         pass
 
-    def test_linear_score_benchmark(self):
+    def test_linear_score_benchmark(self, ip_of_server):
         StopWatch.start()
         StopWatch.stop()
         pass
 
     def test_print_benchmark(self):
         StopWatch.benchmark()
+        StopWatch.clear()
         pass
