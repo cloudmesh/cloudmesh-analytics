b28d43cd39419654ba1cfd840b977a7b1593ef86
Yanting Wan <yanwan@indiana.edu>
2019-11-29 10:58:23 +0800

    write the skeleton code of generating benchmarks

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,14 +1,12 @@
 """
 Generate benchmarks for endpoint functions
 """
+import subprocess
 import numpy as np
 from cloudmesh.common.StopWatch import StopWatch
 
-class TestBenchmark:
-    @pytest.fixture
-    def ip_of_server(self):
-        pass
 
+class TestBenchmark:
     @pytest.fixture
     def generated_data_file(self):
         xs = np.random()
@@ -16,46 +14,47 @@ class TestBenchmark:
         data_file = write(xs, ys)
         return data_file
 
-    def test_file_upload_benchmark(self, generated_data_file, ip_of_server):
+    def test_file_upload_benchmark(self, generated_data_file):
         StopWatch.start()
-        run_command("cms","analytics",...)
+        subprocess.call(['cms', 'analytics', ...])
         StopWatch.stop()
 
-    def test_list_file_benchmark(self, ip_of_server):
+    def test_list_file_benchmark(self):
         StopWatch.start()
-        run_command("cms", "analytics", ...)
+        subprocess.call(['cms', 'analytics', ])
         StopWatch.stop()
 
-    def test_read_file_benchmark(self, generated_data_file, ip_of_server):
+    def test_read_file_benchmark(self, generated_data_file):
         StopWatch.start()
-        run_command("cms", "analytics", ...)
+        subprocess.call(['cms', 'analytics', ])
         StopWatch.stop()
 
-    def test_linear_consturctor_benchmark(self, ip_of_server):
+    def test_linear_consturctor_benchmark(self):
         StopWatch.start()
-        run_command("cms", "analytics", ...)
+        subprocess.call(['cms', 'analytics', 'LinearRegression'])
         StopWatch.stop()
 
-    def test_linear_fit_benchmark(self, ip_of_server):
+    def test_linear_fit_benchmark(self):
         StopWatch.start()
-        run_command("cms", "analytics", ...)
+        subprocess.call(['cms', 'analytics', 'LinearRegression', 'fit'])
         StopWatch.stop()
 
-    def test_linear_get_params_benchmark(self, ip_of_server):
+    def test_linear_get_params_benchmark(self):
         StopWatch.start()
-        run_command("cms", "analytics", ...)
+        subprocess.call(['cms', 'analytics', 'LinearRegression', 'get_params'])
         StopWatch.stop()
 
-    def test_linear_predict_benchmark(self, ip_of_server):
+    def test_linear_predict_benchmark(self):
         StopWatch.start()
-        run_command("cms", "analytics", ...)
+        subprocess.call(['cms', 'analytics', 'LinearRegression', 'predict'])
         StopWatch.stop()
 
-    def test_linear_score_benchmark(self, ip_of_server):
+    def test_linear_score_benchmark(self):
         StopWatch.start()
-        run_command("cms", "analytics", ...)
+        subprocess.call(['cms', 'analytics', 'LinearRegression', 'score'])
         StopWatch.stop()
 
     def test_print_benchmark(self):
         StopWatch.benchmark()
         StopWatch.clear()
+        assert 1==0
