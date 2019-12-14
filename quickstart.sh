5eff29547be2c254d12d38ead0f0d58ce4a3e77c
Gregor von Laszewski <laszewski@gmail.com>
2019-12-13 21:53:40 -0500

    quickstart

---
+++ quickstart.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env sh
+
+cd cloudmesh/analytics/build
+
+cms analytics codegen sklearn linearmodel --class_name=LinearRegression --port=8000
+cms analytics server start detached --cloud=local --class_name=LinearRegression --port=8000
+sleep 3
+#cms analytics LinearRegression
+#cms analytics LinearRegression fit --X="[[1,2]]" --y="[[3,4]]"
+#cms analytics LinearRegression predict --X="[[1,2]]"
+#cms analytics file upload --filename=tests/test_uploaded_files/user_input_data.csv
+#cms analytics LinearRegression predict --X=user_input_data
+#cms analytics file list
+#cms analytics file read --filename=user_input_data
+#cms analytics server stop
+
+cms analytics LinearRegression
+#cms analytics LinearRegression fit X="[[1,2]]" y="[[3,4]]"
+#cms analytics LinearRegression predict X="[[1,2]]"
+cms analytics file upload filename=tests/test_uploaded_files/user_input_data.csv
+#cms analytics LinearRegression predict X=user_input_data
+cms analytics file list
+cms analytics file read filename=user_input_data
+cms analytics server stop
+
+
+
+# cms analytics LinearRegression codegen sklearn linearmodel --port=8002
+
+# cms analytics LinearRegression server start detached --cloud=local
+# sleep 3
+# cms analytics LinearRegression
+# cms analytics LinearRegression fit --X="[[1,2]]" --y="[[3,4]]"
+# cms analytics LinearRegression predict --X="[[1,2]]"
+# cms analytics LinearRegression file upload --filename=tests/test_uploaded_files/user_input_data.csv
+# cms analytics LinearRegression predict --X=user_input_data
+# cms analytics LinearRegression file list
+# cms analytics LinearRegression file read --filename=user_input_data
+# cms analytics LinearRegression
+
+
+
+# if True:
+
+#   eval ("import build.LinearRegression")
+
+cd ../../..
