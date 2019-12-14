e5635c0132729be2897c83c270a1e77d9f1d5010
Gregor von Laszewski <laszewski@gmail.com>
2019-12-14 18:10:19 -0500

    allow creation of the server code in a specific dir

---
+++ a.py
@@ -0,0 +1,17 @@
+import typing
+
+"""
+top doc
+"""
+
+
+def a (x: int, y: float) -> int:
+    """
+    A sample
+
+    :param x: a value
+    :type x: int
+    :return: result
+    :return type: result
+    """
+    return 1
