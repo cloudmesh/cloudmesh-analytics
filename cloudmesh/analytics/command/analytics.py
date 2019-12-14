a0aa1780c2c8254426fe0d30474d0bf34ffac186
Gregor von Laszewski <laszewski@gmail.com>
2019-12-14 05:41:05 -0500

    just use python to create the manual page

--- cloudmesh/analytics/command/analytics.py
+++ cloudmesh/analytics/command/analytics.py
@@ -1,19 +1,141 @@
 from __future__ import print_function
 from cloudmesh.shell.command import command
 from cloudmesh.shell.command import PluginCommand
-from cloudmesh.analytics.api.manager import Manager
 from cloudmesh.common.console import Console
 from cloudmesh.common.util import path_expand
 from cloudmesh.common.debug import VERBOSE
 from cloudmesh.common.run.background import run
 
+from cloudmesh.common.util import writefile
 from cloudmesh.analytics import cms_autoapi
-
+import textwrap
 import json
 import requests
 import os
+import sys
+from pathlib import Path
 import subprocess
 import signal
+from pprint import pprint
+
+
+def manual(service):
+    # Type table
+    type_table = {
+        'matrix': 'array',
+        'array': 'array',
+        'array-like': 'array',
+        'numpy array': 'array',
+        'bool': 'boolean',
+        'int': 'integer',
+        'float': 'number'
+    }
+    import sklearn.linear_model
+    from cloudmesh.analytics.cms_autoapi import SignatureScraper
+    # The module to read
+    module = sklearn.linear_model
+    # The classes to read from the module
+    classes = [service]
+    # If type table is specified, it will read all classes in the module
+    sigs = SignatureScraper().get_signatures(
+        module=module,
+        classes=classes,
+        type_table=type_table)
+
+    pprint (classes)
+
+    pprint(sigs)
+
+    content = [
+        service,
+        len(service) * "=",
+        ""
+    ]
+
+    for counter, entry in sigs.items():
+        print (entry)
+
+        #
+        # create value manual string
+        #
+        command = ["analytics {class_name}".format(**entry)]
+        for parameter in entry["constructor"]:
+            command.append(f"[{parameter}=VALUE]")
+        content.append(' '.join(command))
+        #
+        # create method strings
+        #
+        for name, parameters in entry['members'].items():
+            print (name, parameters)
+            command = ["analytics {class_name} {name}".format(**entry, name=name)]
+            for parameter in parameters:
+                command.append(f"[{parameter}=VALUE]")
+            content.append(' '.join(command))
+        content.append('')
+    print("\n".join(content))
+
+    return ""
+
+
+class Request(object):
+
+    @staticmethod
+    def get_parameters(parameters):
+        data = {}
+        for parameter in parameters:
+            attribute, value = parameter.split("=")
+            try:
+                data[attribute] = json.loads(value)
+            except:
+                data[attribute] = value
+        return data
+
+    @staticmethod
+    def run(service, flag, parameters, command, root_url):
+        data = Request.get_parameters(parameters)
+        name = flag[0]
+        url = f'http://{root_url}/{service}_{name}'
+        payload = {
+            'paras': data
+        }
+        r = requests.post(url, json=payload)
+        return r.text
+
+    @staticmethod
+    def simple_run(service, parameters, root_url):
+        data = Request.get_parameters(parameters)
+        url = f'http://{root_url}/{service}/constructor'
+        print('url:', url)
+        payload = {
+            'paras': data
+        }
+        r = requests.post(url, json=payload)
+        return r.text
+
+    @staticmethod
+    def file_upload(parameters, root_url):
+        data = Request.get_parameters(parameters)
+        url = f'http://{root_url}/file/upload'
+        files = {'file': open(data['filename'], 'rb')}
+        r = requests.post(url, files=files)
+        return r.text
+
+    @staticmethod
+    def file_list(parameters, root_url):
+        data = Request.get_parameters(parameters)
+        url = f'http://{root_url}/file/list'
+        r = requests.get(url)
+        return r.text
+
+    @staticmethod
+    def file_read(parameters, root_url):
+        data = Request.get_parameters(parameters)
+        print('data:', data)
+        filename = data["filename"]
+        url = f'http://{root_url}/file/read/{filename}'
+        r = requests.get(url)
+        return r.text
+
 
 class AnalyticsCommand(PluginCommand):
 
@@ -25,18 +147,15 @@ class AnalyticsCommand(PluginCommand):
 
             Usage:
                 analytics codegen sklearn linearmodel [--class_name=VALUE] [--port=PORT]
-                analytics server start [--cloud=CLOUD]
-                analytics server start detached [--cloud=CLOUD]
+                analytics server start detached [--cloud=CLOUD] [--class_name=VALUE] [--port=PORT]
+                analytics server start [--cloud=CLOUD] [--class_name=VALUE] [--port=PORT]
                 analytics server stop [--cloud=CLOUD]
-                analytics file upload [--filename=FILENAME]
+                analytics file upload PARAMETERS...
                 analytics file list
-                analytics file read [--filename=FILENAME]
-                analytics LinearRegression[--fit_intercept=VALUE] [--normalize=VALUE] [--copy_X=VALUE] [--n_jobs=VALUE] 
-                analytics LinearRegression fit [--X=VALUE]  [--y=VALUE]  [--sample_weight=VALUE] 
-                analytics LinearRegression get_params [--deep=VALUE] 
-                analytics LinearRegression predict [--X=VALUE] 
-                analytics LinearRegression score [--X=VALUE]  [--y=VALUE]  [--sample_weight=VALUE] 
-                
+                analytics file read PARAMETERS...
+                analytics manual SERVICE
+                analytics run SERVICE PARAMETERS...
+                analytics SERVICE
 
             This command manages the cloudmesh analytics server on the given cloud.
             If the cloud is not spified it is run on localhost
@@ -46,31 +165,60 @@ class AnalyticsCommand(PluginCommand):
                                 cloudmesh.yaml file
 
         """
+
+        def set_up_parameters():
+            commands = arguments.PARAMETERS
+            parameters = []
+            flag = []
+            for command in commands:
+                if '=' in command:
+                    parameters.append(command)
+                else:
+                    flag.append(command)
+            print(parameters)
+            print(flag)
+            with open(setting_path, 'r') as settings:
+                settings = json.load(settings)
+                # ip = os.path.join(settings['cloud'][settings['cwd.cloud']]['ip'])
+                ip = os.path.join(settings['cloud']["localhost"]['ip'])
+            return parameters, flag, ip
+
         setting_path = os.path.join(
             (os.path.dirname(__file__)), 'command_setting.json')
 
-        port = arguments["--port"] or 8000
+        port = arguments["--port"] or str(8000)
 
-        if arguments.codegen:
+        if arguments.manual:
+            manual(arguments.SERVICE)
+            return ""
+
+        elif arguments.codegen:
             cms_autoapi.main_generate(arguments['--class_name'], port)
 
-        if arguments.server and arguments.stop:
+        elif arguments.server and arguments.stop:
             with open(setting_path, 'r') as settings:
                 settings = json.load(settings)
 
             print('killing the server')
             server_pid = settings['server_id']
             os.kill(server_pid, signal.SIGKILL)
-            settings['server_id']=""
-            
+            settings['server_id'] = ""
+
             with open(setting_path, 'w') as new_settings:
                 json.dump(settings, new_settings)
 
-        if arguments.server and arguments.start and arguments.detached and arguments['--cloud']:
+        elif arguments.server and arguments.start and arguments.detached and \
+            arguments['--cloud']:
             p = subprocess.Popen(
-                args=['cms', 'analytics', 'server', 'start', '--cloud='+arguments['--cloud']],
+                args=['cms',
+                      'analytics',
+                      'server',
+                      'start',
+                      '--cloud=' + arguments['--cloud'],
+                      '--class_name=' + arguments['--class_name'],
+                      f"--port={port}"],
                 stdout=False)
-            
+
             with open(setting_path, 'r') as settings:
                 settings = json.load(settings)
 
@@ -79,9 +227,32 @@ class AnalyticsCommand(PluginCommand):
             with open(setting_path, 'w') as new_settings:
                 json.dump(settings, new_settings)
 
+        elif arguments.run and arguments.SERVICE:
+            parameters, flag, ip = set_up_parameters()
+            res = Request.run(flag[0], flag[1:], parameters, command, ip)
+            print(res)
+
+        elif arguments.file and arguments.upload:
+            parameters, flag, ip = set_up_parameters()
+            res = Request.file_upload(parameters, ip)
+            print(res)
+
+        elif arguments.file and arguments.list:
+            parameters, flag, ip = set_up_parameters()
+
+            res = Request.file_list(parameters, ip)
+            print(res)
+
+        elif arguments.file and arguments.read:
+            parameters, flag, ip = set_up_parameters()
+            res = Request.file_upload(parameters, ip)
+            print(res)
+
         # Configure current working server
-        if arguments.server and arguments.start and not arguments.detached and arguments['--cloud']:
+        if arguments.server and arguments.start and not arguments.detached and \
+            arguments['--cloud']:
             settings = None
+
             with open(setting_path, 'r') as settings:
                 settings = json.load(settings)
 
@@ -90,126 +261,38 @@ class AnalyticsCommand(PluginCommand):
             with open(setting_path, 'w') as new_settings:
                 json.dump(settings, new_settings)
 
-            from cloudmesh.analytics.build import server
-            server.run_app()
+            service = arguments['--class_name']
+
+            build_path = Path(os.path.dirname(__file__)) / '../build'
+            writefile(f'{build_path}/__init__.py', '')
+
+            # p = f'cloudmesh.analytics.build.{service}_server.Server'
+            p = f'cloudmesh.analytics.build'
+
+            # m = __import__(p, fromlist=[f'{service}_server.Server'])
+
+            m = __import__(p)
+            m = getattr(getattr(m, 'analytics'), 'build')
+            # m = getattr(getattr(getattr(m, 'analytics'), 'build'),f'{service}_server')
+            pprint(m.__dict__)
+            # eval(f'from cloudmesh.analytics.build.{service}_server import run_app')
+            # mod = __import__(f'cloudmesh.analytics.build.{service}_server')
+
+            # server.run_app()
+
+            return
         else:
             with open(setting_path, 'r') as settings:
                 settings = json.load(settings)
                 # ip = os.path.join(settings['cloud'][settings['cwd.cloud']]['ip'])
                 ip = os.path.join(settings['cloud']["localhost"]['ip'])
-                print(run_command(arguments, ip))
+                # print(Request.run(arguments, ip))
+                print("needs to be fixed")
+
+        if arguments.SERVICE:
+            service = arguments.SERVICE
+            parameters, flag, ip = set_up_parameters()
+            res = Request.simple_run(service, parameters, ip)
+            print(res)
 
         return ""
-
-def run_command(arguments, root_url):
-    
-    if arguments.LinearRegression and arguments.fit and ( arguments['--X'] or  arguments['--y'] or  arguments['--sample_weight'] or  True):
-        url = 'http://' + root_url + '/LinearRegression_fit'
-        payload = {'paras': {}}
-        
-        if arguments['--X'] is not None:
-            try:
-                payload['paras']['X']= json.loads(arguments['--X'])
-            except:
-                payload['paras']['X']= arguments['--X']
-        
-        if arguments['--y'] is not None:
-            try:
-                payload['paras']['y']= json.loads(arguments['--y'])
-            except:
-                payload['paras']['y']= arguments['--y']
-        
-        if arguments['--sample_weight'] is not None:
-            try:
-                payload['paras']['sample_weight']= json.loads(arguments['--sample_weight'])
-            except:
-                payload['paras']['sample_weight']= arguments['--sample_weight']
-        
-        r = requests.post(url, json=payload)
-        return r.text
-    
-    if arguments.LinearRegression and arguments.get_params and ( arguments['--deep'] or  True):
-        url = 'http://' + root_url + '/LinearRegression_get_params'
-        payload = {'paras': {}}
-        
-        if arguments['--deep'] is not None:
-            try:
-                payload['paras']['deep']= json.loads(arguments['--deep'])
-            except:
-                payload['paras']['deep']= arguments['--deep']
-        
-        r = requests.post(url, json=payload)
-        return r.text
-    
-    if arguments.LinearRegression and arguments.predict and ( arguments['--X'] or  True):
-        url = 'http://' + root_url + '/LinearRegression_predict'
-        payload = {'paras': {}}
-        
-        if arguments['--X'] is not None:
-            try:
-                payload['paras']['X']= json.loads(arguments['--X'])
-            except:
-                payload['paras']['X']= arguments['--X']
-        
-        r = requests.post(url, json=payload)
-        return r.text
-    
-    if arguments.LinearRegression and arguments.score and ( arguments['--X'] or  arguments['--y'] or  arguments['--sample_weight'] or  True):
-        url = 'http://' + root_url + '/LinearRegression_score'
-        payload = {'paras': {}}
-        
-        if arguments['--X'] is not None:
-            try:
-                payload['paras']['X']= json.loads(arguments['--X'])
-            except:
-                payload['paras']['X']= arguments['--X']
-        
-        if arguments['--y'] is not None:
-            try:
-                payload['paras']['y']= json.loads(arguments['--y'])
-            except:
-                payload['paras']['y']= arguments['--y']
-        
-        if arguments['--sample_weight'] is not None:
-            try:
-                payload['paras']['sample_weight']= json.loads(arguments['--sample_weight'])
-            except:
-                payload['paras']['sample_weight']= arguments['--sample_weight']
-        
-        r = requests.post(url, json=payload)
-        return r.text
-    
-    if arguments.LinearRegression and ( arguments['--fit_intercept'] or  arguments['--normalize'] or  arguments['--copy_X'] or  arguments['--n_jobs'] or  True):
-        url = 'http://'+ root_url + '/LinearRegression_constructor'
-        payload = {'paras': {}}
-        
-        if arguments['--fit_intercept'] is not None:
-            payload['paras']['fit_intercept'] = json.loads(arguments['--fit_intercept'])
-        
-        if arguments['--normalize'] is not None:
-            payload['paras']['normalize'] = json.loads(arguments['--normalize'])
-        
-        if arguments['--copy_X'] is not None:
-            payload['paras']['copy_X'] = json.loads(arguments['--copy_X'])
-        
-        if arguments['--n_jobs'] is not None:
-            payload['paras']['n_jobs'] = json.loads(arguments['--n_jobs'])
-        
-        r = requests.post(url, json=payload)
-        return r.text
-    
-    if arguments.file and arguments.upload and arguments['--filename']:
-        url = 'http://' + root_url + '/file/upload'
-        files = {'file': open(arguments['--filename'], 'rb')}
-        r = requests.post(url, files=files)
-        return r.text
-
-    if arguments.file and arguments.list:
-        url = 'http://' + root_url + '/file/list'
-        r = requests.get(url)
-        return r.text
-
-    if arguments.file and arguments.read and arguments['--filename']:
-        url = 'http://' + root_url + '/file/read/' + arguments['--filename']
-        r = requests.get(url)
-        return r.text
