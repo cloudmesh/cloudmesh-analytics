079a4f31774597f681c50bfe173b75d0e6e3c028
Gregor von Laszewski <laszewski@gmail.com>
2020-02-11 20:43:26 -0500

    split the def example

--- tests/example_def.py
+++ tests/example_def.py
@@ -47,21 +47,3 @@ for f in [
     counter +=1
 
 print (func.__annotations__)
-
-func = LinearRegression
-
-attribute = dir(func)
-
-print (func.__name__)
-print (func.__doc__)
-
-for a in attribute:
-    if "__" not in a:
-        print (">>>>", a)
-        eval(f"print(func.{a})")
-
-# print (func.__annotations__)
-pprint (dir(func))
-
-pprint (dir(func.fit))
-pprint (func.fit.__doc__)
