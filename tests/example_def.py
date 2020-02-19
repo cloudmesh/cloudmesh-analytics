cc3d7f7bff5f6813fef2e602f392f49e1eb291e8
Gregor von Laszewski <laszewski@gmail.com>
2020-02-18 20:46:11 -0500

    add parsing and template examples

--- tests/example_def.py
+++ tests/example_def.py
@@ -1,10 +1,11 @@
 import typing
 from pprint import pprint
 from copy import deepcopy, copy
+from cloudmesh.common.util import readfile
 
 def a (x: int, y: float) -> int:
     """
-    A sample
+    A sample.
 
     :param x: x value
     :type x: int
@@ -25,7 +26,6 @@ for f in [
     func.__name__,
     func.__code__.co_argcount,
     func.__code__.co_varnames,
-    func.__code__.co_argcount,
     func.__code__.co_cellvars,
     func.__code__.co_code,
     func.__code__.co_consts,
@@ -46,3 +46,45 @@ for f in [
     counter +=1
 
 print (func.__annotations__)
+
+template = """
+openapi: 3.0.0
+info:
+  title: {title}
+  description: {description}
+  version: {version}
+servers:
+  - url: http://localhost/cloudmesh/{title}
+    description: Optional server description, e.g. Main (production) server
+paths:
+  /{name}:
+     get:
+        ....
+  /users:
+    get:
+      summary: Returns a list of users.
+      description: Optional extended description in CommonMark or HTML.
+      responses:
+        '200':    # status code
+          description: A JSON array of user names
+          content:
+            application/json:
+              schema: 
+                type: array
+                items: 
+                  type: string
+"""
+
+description = func.__doc__.strip().split("\n")[0]
+version = open('../VERSION','r').read()
+
+spec = template.format(
+    title = func.__name__,
+    name = func.__name__,
+    description = description,
+    version = version
+)
+
+print (spec)
+
+print (version)
